ff50a393422097a69d0f7698e58522f51ed89368
yash4302 <miteswati@gmail.com>
2023-03-14 18:02:11 +0530

    Initial setup of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidePanelComponent } from './components/side-panel/side-panel.component';
+import { AboutUsComponent } from './components/content/about-us/about-us.component';
+import { ReferencesComponent } from './components/content/references/references.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidePanelComponent,
+    AboutUsComponent,
+    ReferencesComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
